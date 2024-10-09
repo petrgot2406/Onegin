425ee866b3eb3722707cbb269bc9d050f3b0b6f3
petrgot2406 <p3trgot@yandex.ru>
2024-10-09 14:32:03 +0300

    Added structures

--- main.cpp
+++ main.cpp
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <assert.h>
 
+struct TheInputFile
+{
+    const char* input_file_name;
+    size_t file_size;
+    size_t str_num;
+};
+
 void Swap_strings(char** string_1, char** string_2);
 int strcmp(char* string_1, char* string_2);
-void bubble_sort(char** strings, size_t str_num);
-size_t num_of_symbols_in_file(const char* filename);
-size_t num_of_strings_in_file(const char* filename);
-size_t max_strlen_of_file(const char* filename);
-void read_file_to_buffer(const char* filename, size_t filesize, char* buffer);
-void put_strlen_for_all_strings(char* buffer, size_t filesize, size_t* strlen);
-void put_pointers_to_strings(char* buffer, size_t filesize, size_t str_num, char** strings);
-void print_strings(char** strings, size_t* strlen, size_t str_num);
+void bubble_sort(char** strings, TheInputFile input_file);
+size_t num_of_symbols_in_file(TheInputFile input_file);
+size_t num_of_strings_in_file(TheInputFile input_file);
+size_t max_strlen_of_file(TheInputFile input_file);
+void read_file_to_buffer(TheInputFile input_file, char* buffer);
+void put_strlen_for_all_strings(char* buffer, TheInputFile input_file, size_t* strlen);
+void put_pointers_to_strings(char* buffer, TheInputFile input_file, char** strings);
+void print_strings(char** strings, size_t* strlen, TheInputFile input_file);
 //void read_text_from_buffer(char* buffer, char** strings);
 
 int main(int argc, char* argv[])
@@ -25,45 +32,51 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    const char* input_file_name = "onegin.txt";
+    struct TheInputFile input_file = {};
+
+    input_file.input_file_name = "onegin.txt";
+    input_file.str_num = num_of_strings_in_file(input_file);
+    input_file.file_size = num_of_symbols_in_file(input_file);
     //FILE* outputfile = fopen("oneginoutput.txt", "w");
     //FILE* inputfile = fopen("onegin.txt", "r");
 
-    size_t str_num = num_of_strings_in_file(input_file_name);
-    size_t max_str_len = max_strlen_of_file(input_file_name);
-    size_t filesize = num_of_symbols_in_file(input_file_name);
+    /*
+    size_t str_num = num_of_strings_in_file(input_file.input_file_name);
+    size_t file_size = num_of_symbols_in_file(input_file.input_file_name);
+    */
 
     //size_t max_fsize = str_num * max_str_len;
     //char* buffer = (char*)calloc(max_fsize, sizeof(char));
-    char* buffer = (char*)calloc(filesize, sizeof(char));
-    char** strings = (char**)calloc(str_num + 1, sizeof(char*));
-    size_t* strlen = (size_t*)calloc(str_num, sizeof(size_t));
+    char* buffer = (char*)calloc(input_file.file_size, sizeof(char));
+    char** strings = (char**)calloc(input_file.str_num + 1, sizeof(char*));
+    size_t* strlen = (size_t*)calloc(input_file.str_num, sizeof(size_t));
 
-    printf("There are %d strings\n", str_num);
+    printf("There are %d strings\n", input_file.str_num);
+    size_t max_str_len = max_strlen_of_file(input_file);
     printf("Max length of strings is %d\n", max_str_len);
 
-    read_file_to_buffer(input_file_name, filesize, buffer);
+    read_file_to_buffer(input_file, buffer);
 
     //fprintf(outputfile, "%s", buffer);
     //printf("%s\n", buffer);
 
-    put_strlen_for_all_strings(buffer, filesize, strlen);
+    put_strlen_for_all_strings(buffer, input_file, strlen);
 
-    for (size_t i = 0; i < str_num; i++)
+    for (size_t i = 0; i < input_file.str_num; i++)
     {
         printf("%d ", strlen[i]);
     }
     printf("\n");
 
-    put_pointers_to_strings(buffer, filesize, str_num, strings);
+    put_pointers_to_strings(buffer, input_file, strings);
 
     printf("Original text:\n");
-    print_strings(strings, strlen, str_num);
+    print_strings(strings, strlen, input_file);
 
-    bubble_sort(strings, str_num);
+    bubble_sort(strings, input_file);
 
     printf("Sorted text:\n");
-    print_strings(strings, strlen, str_num);
+    print_strings(strings, strlen, input_file);
 
     free(strlen);
     free(buffer);
@@ -102,10 +115,10 @@ int strcmp(char* string_1, char* string_2)
     return 0;
 }
 
-void bubble_sort(char** strings, size_t str_num) {
-    for (size_t i = 0; i < str_num - 1; i++)
+void bubble_sort(char** strings, TheInputFile input_file) {
+    for (size_t i = 0; i < input_file.str_num - 1; i++)
     {
-        for (size_t j = 0; j < str_num - i - 1; j++)
+        for (size_t j = 0; j < input_file.str_num - i - 1; j++)
         {
             if (strcmp(strings[j], strings[j + 1]) > 0)
             {
@@ -115,10 +128,10 @@ void bubble_sort(char** strings, size_t str_num) {
     }
 }
 
-size_t num_of_symbols_in_file(const char* filename)
+size_t num_of_symbols_in_file(TheInputFile input_file)
 {
-    assert(filename != NULL);
-    FILE *fptr = fopen(filename, "r");
+    assert(input_file.input_file_name != NULL);
+    FILE *fptr = fopen(input_file.input_file_name, "r");
     size_t counter = 0;
     while (getc(fptr) != EOF)
     {
@@ -128,10 +141,10 @@ size_t num_of_symbols_in_file(const char* filename)
     return counter;
 }
 
-size_t num_of_strings_in_file(const char* filename)
+size_t num_of_strings_in_file(TheInputFile input_file)
 {
-    assert(filename != NULL);
-    FILE *fptr = fopen(filename, "r");
+    assert(input_file.input_file_name != NULL);
+    FILE *fptr = fopen(input_file.input_file_name, "r");
     size_t counter = 0;
     int ch = getc(fptr);
     while (ch != EOF)
@@ -146,10 +159,10 @@ size_t num_of_strings_in_file(const char* filename)
     return counter;
 }
 
-size_t max_strlen_of_file(const char* filename)
+size_t max_strlen_of_file(TheInputFile input_file)
 {
-    assert(filename != NULL);
-    FILE *fptr = fopen(filename, "r");
+    assert(input_file.input_file_name != NULL);
+    FILE *fptr = fopen(input_file.input_file_name, "r");
     size_t max_counter = 0;
     size_t counter = 0;
     int ch = getc(fptr);
@@ -170,23 +183,23 @@ size_t max_strlen_of_file(const char* filename)
     return max_counter;
 }
 
-void read_file_to_buffer(const char* filename, size_t filesize, char* buffer)
+void read_file_to_buffer(TheInputFile input_file, char* buffer)
 {
     assert(buffer != NULL);
-    assert(filename != NULL);
-    FILE* fptr = fopen(filename, "r");
+    assert(input_file.input_file_name != NULL);
+    FILE* fptr = fopen(input_file.input_file_name, "r");
     if (fptr)
     {
-        fread(buffer, sizeof(char), filesize, fptr);
+        fread(buffer, sizeof(char), input_file.file_size, fptr);
         fclose(fptr);
     }
 }
 
-void put_strlen_for_all_strings(char* buffer, size_t filesize, size_t* strlen)
+void put_strlen_for_all_strings(char* buffer, TheInputFile input_file, size_t* strlen)
 {
     size_t num_of_the_str = 0;
     size_t count_sym_in_str = 0;
-    for (size_t i = 0; i < filesize; i++)
+    for (size_t i = 0; i < input_file.file_size; i++)
     {
         count_sym_in_str++;
         if (buffer[i] == '\n' ||  buffer[i] == '\0' || buffer[i] == EOF)
@@ -198,24 +211,24 @@ void put_strlen_for_all_strings(char* buffer, size_t filesize, size_t* strlen)
     }
 }
 
-void put_pointers_to_strings(char* buffer, size_t filesize, size_t str_num, char** strings)
+void put_pointers_to_strings(char* buffer, TheInputFile input_file, char** strings)
 {
     size_t num_of_the_str = 1;
     strings[0] = &buffer[0];
-    for (size_t i = 1; i < filesize; i++)
+    for (size_t i = 1; i < input_file.file_size; i++)
     {
         if (buffer[i] == '\n')
         {
-            assert(num_of_the_str <= str_num);
+            assert(num_of_the_str <= input_file.str_num);
             strings[num_of_the_str] = &buffer[i + 1];
             num_of_the_str++;
         }
     }
 }
 
-void print_strings(char** strings, size_t* strlen, size_t str_num)
+void print_strings(char** strings, size_t* strlen, TheInputFile input_file)
 {
-    for (size_t i = 0; i < str_num; i++)
+    for (size_t i = 0; i < input_file.str_num; i++)
     {
         for (size_t j = 0; j < strlen[i]; j++)
         {
